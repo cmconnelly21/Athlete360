2c51ddbfdaa3a2ba5c9f9cdcce4cb1b8c39aea99
cmconnelly21 <cmconnelly21@gmail.com>
2019-06-24 15:43:54 -0400

    Updated upload script to move old files to archive

--- HPCCupload.ps1
+++ HPCCupload.ps1
@@ -1,7 +1,9 @@
 $d=Get-Date -UFormat %Y-%m-%d
+$a = [DateTime]::Today.AddDays(-30).ToString("yyyy-MM-dd")
 
-ssh -i C:\Users\cmconnelly\Desktop\NCSU\id_rsa ec2-user@18.191.244.70 "mkdir /var/lib/HPCCSystems/mydropzone/Athlete360/$d"
+ssh -i C:\Users\cmconnelly\Desktop\NCSU\id_rsa ec2-user@18.191.244.70 "mkdir /var/lib/HPCCSystems/mydropzone/Athlete360/$d;mv /var/lib/HPCCSystems/mydropzone/Athlete360/$a /var/lib/HPCCSystems/mydropzone/A360archive/$a"
 
 $d=Get-Date -UFormat %Y-%m-%d
+$a = [DateTime]::Today.AddDays(-30).ToString("yyyy-MM-dd")
 
 scp -i C:\Users\cmconnelly\Desktop\NCSU\id_rsa C:\Users\cmconnelly\Desktop\Athlete360data\*.csv ec2-user@18.191.244.70:/var/lib/HPCCSystems/mydropzone/Athlete360/$d
